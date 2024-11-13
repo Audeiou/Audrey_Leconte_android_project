a2dc35590ea2304647ab6168c69437e39a83b73a
Audeiou <nacre@LAPTOP-KN7PQSMG>
2024-11-13 11:33:56 +0100

    calculatrice marche !

--- TP1/app/src/main/java/fr/ensicaen/tp1/MainActivity.java
+++ TP1/app/src/main/java/fr/ensicaen/tp1/MainActivity.java
@@ -3,6 +3,7 @@ package fr.ensicaen.tp1;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,19 +14,14 @@ import androidx.core.view.WindowInsetsCompat;
 import net.objecthunter.exp4j.Expression;
 import net.objecthunter.exp4j.ExpressionBuilder;
 
-public class MainActivity extends AppCompatActivity {
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 
-    private Button exit;  // tout en anglais
-    private Button number0;
-    private Button number1;
-    private Button number2;
-    private Button number3;
-    private Button number4;
-    private Button number5;
-    private Button number6;
-    private Button number7;
-    private Button number8;
-    private Button number9;
+//interroger une API avec thread + utiliser base de données
+// pour API : Android ROOM
+// fond d'écran ? qui change avec flèches
+
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,22 +34,76 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        /*String operations = "";
-        Expression expression = new ExpressionBuilder(operations).build();
-        double result = expression.evaluate();
+        Button number0 = findViewById(R.id.button0);
+        Button number1 = findViewById(R.id.button1);
+        Button number2 = findViewById(R.id.button2);
+        Button number3 = findViewById(R.id.button3);
+        Button number4 = findViewById(R.id.button4);
+        Button number5 = findViewById(R.id.button5);
+        Button number6 = findViewById(R.id.button6);
+        Button number7 = findViewById(R.id.button7);
+        Button number8 = findViewById(R.id.button8);
+        Button number9 = findViewById(R.id.button9);
 
-        number0 = findViewById(R.id.button0);
-        number1 = findViewById(R.id.button1);
-        number2 = findViewById(R.id.button2);
-        number3 = findViewById(R.id.button3);
-        number4 = findViewById(R.id.button4);
-        number5 = findViewById(R.id.button5);
-        number6 = findViewById(R.id.button6);
-        number7 = findViewById(R.id.button7);
-        number8 = findViewById(R.id.button8);
-        number9 = findViewById(R.id.button9);*/
+        Button plus = findViewById(R.id.plusButton);
+        Button minus = findViewById(R.id.minusButton);
+        Button times = findViewById(R.id.timesButton);
+        Button divide = findViewById(R.id.divideButton);
 
-        exit = findViewById(R.id.exitButton);
+        Button equal = findViewById(R.id.equalButton);
+        Button erase = findViewById(R.id.eraseButton);
+
+        TextView resultText = findViewById(R.id.result);
+        TextView formulaText = findViewById(R.id.formula);
+
+        ArrayList<Button> numbers = new ArrayList<>();
+        numbers.add(number0);
+        numbers.add(number1);
+        numbers.add(number2);
+        numbers.add(number3);
+        numbers.add(number4);
+        numbers.add(number5);
+        numbers.add(number6);
+        numbers.add(number7);
+        numbers.add(number8);
+        numbers.add(number9);
+
+        for (Button button : numbers) {
+            button.setOnClickListener(view -> {
+                if (!formulaText.getText().toString().equals("0")) {
+                    formulaText.setText(formulaText.getText().toString() + button.getText().toString());
+                } else {
+                    formulaText.setText(button.getText().toString());
+                }
+            });
+        }
+
+        ArrayList<Button> operations = new ArrayList<>();
+        operations.add(divide);
+        operations.add(plus);
+        operations.add(minus);
+        operations.add(times);
+
+        for (Button button : operations) {
+            button.setOnClickListener(view -> {
+                formulaText.setText(formulaText.getText().toString() + button.getText().toString());
+            });
+        }
+
+        equal.setOnClickListener( view -> {
+            String formula = formulaText.getText().toString();
+            Expression expression = new ExpressionBuilder(formula).build();
+            double result = expression.evaluate();
+            String resultString = String.valueOf(result);
+            resultText.setText(resultString);
+        });
+
+        erase.setOnClickListener( view -> {
+            formulaText.setText("0");
+            resultText.setText("0");
+        });
+
+        Button exit = findViewById(R.id.exitButton);
         exit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
